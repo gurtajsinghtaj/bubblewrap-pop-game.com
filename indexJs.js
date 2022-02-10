0ae91461a4d7748983687643f035faca3b9efbbe
gurtajsinghtaj <94396296+gurtajsinghtaj@users.noreply.github.com>
2022-02-10 23:39:39 +0530

    Add files via upload

---
+++ indexJs.js
@@ -0,0 +1,574 @@
+function changeImage1() {
+    var image = document.getElementById('myImage1');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+
+    }
+}
+
+function changeImage2() {
+    var image = document.getElementById('myImage2');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage3() {
+    var image = document.getElementById('myImage3');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage4() {
+    var image = document.getElementById('myImage4');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage5() {
+    var image = document.getElementById('myImage5');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage6() {
+    var image = document.getElementById('myImage6');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage7() {
+    var image = document.getElementById('myImage7');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage8() {
+    var image = document.getElementById('myImage8');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage9() {
+    var image = document.getElementById('myImage9');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage10() {
+    var image = document.getElementById('myImage10');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage11() {
+    var image = document.getElementById('myImage11');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage12() {
+    var image = document.getElementById('myImage12');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage13() {
+    var image = document.getElementById('myImage13');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage14() {
+    var image = document.getElementById('myImage14');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage15() {
+    var image = document.getElementById('myImage15');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage16() {
+    var image = document.getElementById('myImage16');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage17() {
+    var image = document.getElementById('myImage17');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage18() {
+    var image = document.getElementById('myImage18');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage19() {
+    var image = document.getElementById('myImage19');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage20() {
+    var image = document.getElementById('myImage20');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage21() {
+    var image = document.getElementById('myImage21');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage22() {
+    var image = document.getElementById('myImage22');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage23() {
+    var image = document.getElementById('myImage23');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage24() {
+    var image = document.getElementById('myImage24');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage25() {
+    var image = document.getElementById('myImage25');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage26() {
+    var image = document.getElementById('myImage26');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage26() {
+    var image = document.getElementById('myImage26');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage27() {
+    var image = document.getElementById('myImage27');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage28() {
+    var image = document.getElementById('myImage28');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage29() {
+    var image = document.getElementById('myImage29');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage30() {
+    var image = document.getElementById('myImage30');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage31() {
+    var image = document.getElementById('myImage31');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage32() {
+    var image = document.getElementById('myImage32');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage33() {
+    var image = document.getElementById('myImage33');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage34() {
+    var image = document.getElementById('myImage34');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage35() {
+    var image = document.getElementById('myImage35');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage36() {
+    var image = document.getElementById('myImage36');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage37() {
+    var image = document.getElementById('myImage37');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage38() {
+    var image = document.getElementById('myImage38');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage39() {
+    var image = document.getElementById('myImage39');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage40() {
+    var image = document.getElementById('myImage40');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage41() {
+    var image = document.getElementById('myImage41');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage42() {
+    var image = document.getElementById('myImage42');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage43() {
+    var image = document.getElementById('myImage43');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage44() {
+    var image = document.getElementById('myImage44');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage45() {
+    var image = document.getElementById('myImage45');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage46() {
+    var image = document.getElementById('myImage46');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage47() {
+    var image = document.getElementById('myImage47');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage48() {
+    var image = document.getElementById('myImage48');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage49() {
+    var image = document.getElementById('myImage49');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage50() {
+    var image = document.getElementById('myImage50');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage51() {
+    var image = document.getElementById('myImage51');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage52() {
+    var image = document.getElementById('myImage52');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage53() {
+    var image = document.getElementById('myImage53');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage54() {
+    var image = document.getElementById('myImage54');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage55() {
+    var image = document.getElementById('myImage55');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function changeImage56() {
+    var image = document.getElementById('myImage56');
+    if (image.src.match("unpop.jpg")) {
+        image.src = "poped.jpg";
+        var audio = document.getElementById("audio");
+        audio.play();
+        navigator.vibrate(100)
+    }
+}
+
+function resetbubble() {
+    window.location.reload();
+}
